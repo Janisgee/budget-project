7ef6296f6901643f5bfb29408ced38a5ce7edf7d
janisgee <janisgeegee@gmail.com>
2023-09-20 14:13:28 +0800

    Added data validation in transactionSchema

--- server/models/transactionModel.js
+++ server/models/transactionModel.js
@@ -12,15 +12,25 @@ const transactionSchema = new mongoose.Schema({
     type: Date,
     default: new Date().toISOString(),
     required: [true, 'A transaction must have a date.'],
+    min: [
+      Date.UTC('2020'),
+      'A transaction date must not older than year 2020.',
+    ],
+    max: [Date.UTC('2300'), 'A transaction date must not more than year 2300'],
   },
   tag: {
     type: String,
     required: [true, 'A transaction must have a tag.'],
     trim: true,
+    maxLength: [30, 'A transaction note must have less than 20 words.'],
   },
   type: {
     type: String,
     required: [true, 'A transaction must have a type.'],
+    enum: {
+      values: ['Expense', 'Income'],
+      message: 'A transaction is either: "Expense" or "Income".',
+    },
   },
   value: {
     type: Number,
@@ -29,21 +39,18 @@ const transactionSchema = new mongoose.Schema({
   note: {
     type: String,
     trim: true,
+    maxLength: [50, 'A transaction note must have less than 40 words.'],
   },
 });
 
+//DOCUMENT MIDDLEWARE
 transactionSchema.pre('save', function (next) {
   console.log('Pre Schema');
   this.slug = slugify(this.category, { lower: true });
   next();
 });
 
-// transactionSchema.post('save', function (doc, next) {
-//   console.log('Post Schema');
-
-//   console.log(doc);
-//   next();
-// });
+// QUERY MIDDLEWARE
 
 const Transaction = mongoose.model('Transaction', transactionSchema);
 module.exports = Transaction;
