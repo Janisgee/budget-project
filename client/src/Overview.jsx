270bbdd2a9170144f426dd18b5ee2e4c36832c2e
janisgee <janisgeegee@gmail.com>
2023-10-18 18:47:29 +0800

    Added list on Overview content page

--- client/src/Overview.jsx
+++ client/src/Overview.jsx
@@ -6,8 +6,31 @@ export default function Overview() {
     <div className="overview-container">
       <div className="container">
         <p>Balance on July 2023</p>
-        <div>
+        <div className="overview-content">
           <PieChart />
+
+          <ul className="list-items">
+            <li className="list">
+              <h4>🍽️ Eating Out</h4>
+              <p>-$20.00</p>
+            </li>
+            <li className="list">
+              <h4>🍽️ Eating Out</h4>
+              <p>-$20.00</p>
+            </li>
+            <li className="list">
+              <h4>🍽️ Eating Out</h4>
+              <p>-$20.00</p>
+            </li>
+            <li className="list">
+              <h4>🍽️ Eating Out</h4>
+              <p>-$20.00</p>
+            </li>
+            <li className="list">
+              <h4>🍽️ Eating Out</h4>
+              <p>-$20.00</p>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
