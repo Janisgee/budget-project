8cee1b00f6561f9a421797aec50639ee6f532f86
janisgee <janisgeegee@gmail.com>
2023-10-18 20:34:53 +0800

    Added sort button on Overview page

--- client/src/Overview.jsx
+++ client/src/Overview.jsx
@@ -4,34 +4,40 @@ import './Overview.css';
 export default function Overview() {
   return (
     <div className="overview-container">
-      <div className="container">
+      <div className="overview-heading">
         <p>Balance on July 2023</p>
-        <div className="overview-content">
-          <PieChart />
-
-          <ul className="list-items">
-            <li className="list">
-              <h4>🍽️ Eating Out</h4>
-              <p>-$20.00</p>
-            </li>
-            <li className="list">
-              <h4>🍽️ Eating Out</h4>
-              <p>-$20.00</p>
-            </li>
-            <li className="list">
-              <h4>🍽️ Eating Out</h4>
-              <p>-$20.00</p>
-            </li>
-            <li className="list">
-              <h4>🍽️ Eating Out</h4>
-              <p>-$20.00</p>
-            </li>
-            <li className="list">
-              <h4>🍽️ Eating Out</h4>
-              <p>-$20.00</p>
-            </li>
-          </ul>
+        <div id="sort-by-button">
+          <select name="sort-by" className="btn" id="sort-by">
+            <option value="Income">Income</option>
+            <option value="Expense">Expense</option>
+          </select>
         </div>
+      </div>
+      <div className="overview-content">
+        <PieChart />
+
+        <ul className="list-items">
+          <li className="list">
+            <h4>🍽️ Eating Out</h4>
+            <p>-$20.00</p>
+          </li>
+          <li className="list">
+            <h4>🍽️ Eating Out</h4>
+            <p>-$20.00</p>
+          </li>
+          <li className="list">
+            <h4>🍽️ Eating Out</h4>
+            <p>-$20.00</p>
+          </li>
+          <li className="list">
+            <h4>🍽️ Eating Out</h4>
+            <p>-$20.00</p>
+          </li>
+          <li className="list">
+            <h4>🍽️ Eating Out</h4>
+            <p>-$20.00</p>
+          </li>
+        </ul>
       </div>
     </div>
   );
--- client/src/Profile.jsx
+++ client/src/Profile.jsx
@@ -4,9 +4,26 @@ export default function Profile() {
   return (
     <div className="profile-container">
       <div className="profile">
-        <p>Welcome back, Cameron !</p>
+        <h3>Welcome back, Cameron !</h3>
         <img src="https://i.pravatar.cc/50" alt="Cameron" />
       </div>
+      <ul className="total">
+        <li className="list">
+          <h3>🐖 Wallet</h3>
+          <p>+$13,265.21</p>
+        </li>
+        <li className="list">
+          <h3>💸 All Expense</h3>
+          <p>-$8,326.21</p>
+        </li>
+        <li className="list">
+          <h3>💵 All Income</h3>
+          <p>+$13,26.21</p>
+        </li>
+      </ul>
+
+      <h3> </h3>
+      <h3> </h3>
     </div>
   );
 }
