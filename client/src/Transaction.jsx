dcc24d3e45825b31761357323213c499924453a6
janisgee <janisgeegee@gmail.com>
2023-10-21 22:49:36 +0800

    Add transaction model

--- client/src/Transaction.jsx
+++ client/src/Transaction.jsx
@@ -1,10 +1,65 @@
 import EachDateTransaction from './EachDateTransaction';
 import './Transaction.css';
+import './css/form.css';
 
 export default function Transaction() {
   return (
     <div className="transaction">
       <div className="transaction-container">
+        <div className="transaction-modal">
+          <form
+            action=""
+            method="get"
+            class="form-transaction transaction-modal-content"
+          >
+            <div className="flex-space-between">
+              <span>Edit Transaction</span>
+              <button className="closeButton">
+                <span>&times;</span>
+              </button>
+            </div>
+            <div>
+              <label for="type">Type: </label>
+              <select name="type" id="type" required>
+                <option value="">--Please choose a type</option>
+                <option vallue="income">Income</option>
+                <option vallue="expense">Expense</option>
+              </select>
+            </div>
+            <div>
+              <label for="value">Value: </label>
+              <input type="number" id="value" placeholder="amount" />
+            </div>
+            <div>
+              <label for="category">Category: </label>
+              <select name="category" id="category" required>
+                <option value="">--Please choose a category</option>
+                <option vallue=""></option>
+              </select>
+            </div>
+            <div>
+              <label for="date">Date: </label>
+              <input type="date" name="date" id="date" required />
+            </div>
+            <div>
+              <label for="tag">Tag: </label>
+              <input type="tag" id="tag" placeholder="note" />
+            </div>
+            <div className="flex-space-between">
+              <button className="btn">Save Transaction</button>
+              <button className="btn ">Delete Transaction</button>
+            </div>
+          </form>
+        </div>
+        <div className="transaction-modal  confirm-delete-modal">
+          <div className="confirm-delete-content">
+            <h2>Are you sure you want to delete this transaction?</h2>
+            <span className="">
+              <button className="btn btn-red">Delete</button>
+              <button className="btn">Cancel</button>
+            </span>
+          </div>
+        </div>
         <span className="transaction-heading">
           <h3>Cameron's Account</h3>
           <select name="transaction-sort-by-month" className="btn" id="sort-by">
