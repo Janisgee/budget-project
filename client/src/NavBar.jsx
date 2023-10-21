453eb821c1203be669371496483da71b4bbbdb08
janisgee <janisgeegee@gmail.com>
2023-10-21 22:50:25 +0800

    Add login and signup nav

--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -5,8 +5,12 @@ export default function navBar() {
     <nav className="navbar flex-space-between">
       <div className="logo">💰Money Tracker</div>
       <div className="btn-group">
-        <button className="btn btn-login">Login</button>
-        <button className="btn btn-signup">Sign Up</button>
+        <a href="/login">
+          <button className="btn btn-login">Login</button>
+        </a>
+        <a href="/signup">
+          <button className="btn btn-signup">Sign Up</button>
+        </a>
       </div>
     </nav>
   );
