28736924e14fbdf459ce46a87c8ac61bd3221e26
janisgee <janisgeegee@gmail.com>
2023-10-24 09:37:17 +0800

    Added toggle effect on navbarafterlogin

--- client/src/components/NavBarAfterLogin.jsx
+++ client/src/components/NavBarAfterLogin.jsx
@@ -1,17 +1,31 @@
+import { useState } from 'react';
 import './css/btn.css';
 import { Link } from 'react-router-dom';
 
 export default function NavBarAfterLogin() {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggleChange = () => {
+    setToggle(!toggle);
+  };
   return (
     <nav className="navBarAfterLogin">
       <Link to="/user/overview" className="logo navLink link">
         💰Money Tracker
       </Link>
       <div className="btn-group">
-        <Link to="/user/overview" className="link btn-overview active">
+        <Link
+          to="/user/overview"
+          className={toggle ? 'link btn-overview active' : 'link btn-overview'}
+          onClick={handleToggleChange}
+        >
           Overview
         </Link>
-        <Link to="/user/transaction" className="link btn-overview">
+        <Link
+          to="/user/transaction"
+          className={toggle ? 'link btn-overview ' : 'link btn-overview active'}
+          onClick={handleToggleChange}
+        >
           Transaction
         </Link>
         <Link to="/" className="link btn">
