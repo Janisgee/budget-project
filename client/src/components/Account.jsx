c0dc9d84ebb512e2a224bdfd5cab07e125daa3f4
janisgee <janisgeegee@gmail.com>
2023-11-06 18:24:49 +0800

    Get account form data and update

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,55 +1,108 @@
+import { useState, useRef } from 'react';
+import { useAuth } from '../contexts/authContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGear } from '@fortawesome/free-solid-svg-icons';
+import { patchUpdateMe } from '../js/api-service';
 
 export default function Account() {
+  const { user, updateUser } = useAuth();
+  const [file, setFile] = useState(user.photo);
+  const settingUserFormRef = useRef();
+  const uploadPhoto = document.getElementById('photo');
+  const userIcon = document.getElementById('userIcon');
+
+  console.log(user);
+  async function getFormDetailsAndUpdate() {
+    const formData = new FormData(settingUserFormRef.current);
+    const data = Object.fromEntries(formData.entries());
+    console.log(file);
+
+    if (file) {
+      data.photo = file;
+    } else {
+      data.photo = user.photo;
+    }
+    console.log(file);
+
+    //Update user information to server
+    await patchUpdateMe(data);
+
+    //Update user information UI
+    updateUser(user, data);
+
+    setFile('');
+  }
+
+  function handlePhotoChange(e) {
+    const newPhotoName = e.target.files[0].name;
+    setFile(newPhotoName);
+    e.preventDefault();
+    if (uploadPhoto) {
+      const [file] = uploadPhoto.files;
+      if (file) {
+        userIcon.src = URL.createObjectURL(file);
+      }
+    }
+  }
+
+  function handleUpdate(e) {
+    e.preventDefault();
+    getFormDetailsAndUpdate();
+  }
+
   return (
     <div className="wrapper">
       <div className="setting-heading">
         <FontAwesomeIcon icon={faGear} /> &nbsp; Your Account Settings
       </div>
-      <form className="settingForm">
+      <form className="settingForm" ref={settingUserFormRef}>
         <div className="settingFormGroup">
-          <label forHtml="settingName" className="settingForm__label">
+          <label forhtml="settingName" className="settingForm__label">
             Name
           </label>
           <input
             type="text"
             className="settingform__input"
-            name="settingName"
-            placeholder=""
+            name="name"
+            defaultValue={user.name}
+            id="name"
             required
           ></input>
         </div>
         <div className="settingFormGroup">
-          <label forHtml="settingEmail" className="settingForm__label">
+          <label forhtml="settingEmail" className="settingForm__label">
             Email
           </label>
           <input
-            type="text"
+            type="email"
             className="settingform__input"
-            name="settingEmail"
-            placeholder=""
+            name="email"
+            defaultValue={user.email}
+            id="email"
             required
           ></input>
         </div>
+        <hr />
         <div className="settingFormGroup">
           <div className="settingForm_End">
             <div className="imageSet">
-              {/* <img src="" alt="" /> */}
               <img
-                src="https://i.pravatar.cc/50"
+                src={`/img/users/${user.photo}`}
                 className="settingform_UserImage"
-                alt="Cameron"
+                id="userIcon"
+                alt={user.name}
               />
               <input
                 type="file"
                 className="settingform__ImageUpload"
-                name="settingImage"
+                name="photo"
+                id="photo"
                 accept="image/*"
-              ></input>
-              <label forHtml="settingImage">Choose new photo</label>
+                onChange={handlePhotoChange}
+              />
+              <label for="photo">Choose new photo</label>
             </div>
-            <a class="link btn" href="/">
+            <a className="link btn" href="/" onClick={handleUpdate}>
               Save&nbsp;Settings
             </a>
           </div>
