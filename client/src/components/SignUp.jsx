8f76a2cb5e0b023b0076705295c8ed4225b81c16
janisgee <janisgeegee@gmail.com>
2023-11-09 10:46:54 +0800

    Connect sign up form to server api

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,28 +1,88 @@
+import { useRef } from 'react';
 import './css/form.css';
+import { showAlert } from '../js/alerts';
+import { signup } from '../js/api-service';
+import { useNavigate } from 'react-router-dom';
 
 export default function SignUp() {
+  const signupUserFormRef = useRef();
+  const navigate = useNavigate();
+  async function handleSignupSubmit(e) {
+    e.preventDefault();
+
+    const formData = new FormData(signupUserFormRef.current);
+    const userName = formData.get('name');
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const passwordConfirm = formData.get('passwordConfirm');
+
+    if (!userName || !email || !password || !passwordConfirm) {
+      showAlert(
+        'error',
+        'Missing information, please fill in all the blank from the form.',
+        4
+      );
+      return;
+    }
+
+    const data = {
+      name: userName,
+      email: email,
+      password: password,
+      passwordConfirm: passwordConfirm,
+    };
+
+    try {
+      await signup(data);
+      window.setTimeout(() => {
+        navigate('/user/overview');
+      }, 2000);
+    } catch (err) {
+      signupUserFormRef.current.reset();
+    }
+  }
+
   return (
     <div className="container">
-      <form className="form signupForm">
+      <form
+        className="form signupForm"
+        onSubmit={handleSignupSubmit}
+        ref={signupUserFormRef}
+      >
         <h2>✒️ Sign Up</h2>
         <div className="row">
-          <label htmlFor="name">User Name</label>
-          <input type="text" id="name" />
+          <label htmlFor="userName">User Name</label>
+          <input type="text" id="userName" name="name" defaultValue="Janis" />
         </div>
 
         <div className="row">
           <label htmlFor="sign-up-email">Email address</label>
-          <input type="text" id="sign-up-email" />
+          <input
+            type="text"
+            id="sign-up-email"
+            name="email"
+            defaultValue="janisgeegee@gmail.com"
+          />
         </div>
 
         <div className="row">
           <label htmlFor="sign-up-password">Password</label>
-          <input type="password" id="sign-up-password" />
+          <input
+            type="password"
+            id="sign-up-password"
+            name="password"
+            defaultValue="pass1234"
+          />
         </div>
 
         <div className="row">
-          <label htmlFor="confirmPassword">Confirm Password</label>
-          <input type="password" id="confirmPassword" />
+          <label htmlFor="sign-up-passwordConfirm">Confirm Password</label>
+          <input
+            type="password"
+            id="sign-up-passwordConfirm"
+            name="passwordConfirm"
+            defaultValue="pass1234"
+          />
         </div>
 
         <div>
