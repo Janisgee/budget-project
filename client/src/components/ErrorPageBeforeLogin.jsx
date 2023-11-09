ec02c80c90cb86bf80b27c462428b34ab21ffc99
janisgee <janisgeegee@gmail.com>
2023-11-09 19:25:29 +0800

    Add Error page button to redirect to main page

--- client/src/components/ErrorPageBeforeLogin.jsx
+++ client/src/components/ErrorPageBeforeLogin.jsx
@@ -1,11 +1,21 @@
+import { Link } from 'react-router-dom';
 import './App.css';
+import './css/btn.css';
 import './css/error.css';
+import NavBar from './NavBar';
 
-export default function Error(errMsg) {
+export default function Error() {
   return (
-    <div className="error">
-      <h2 className="errorTitle">Uh oh! Something went wrong!😢 </h2>
-      <div className="errorMsg">error message</div>
+    <div className="App">
+      <NavBar />
+      <div className="error">
+        <h2 className="errorTitle">Uh oh! Something went wrong!😢 </h2>
+        <p>
+          <Link to="/" className="btn link">
+            Refresh to main page
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
