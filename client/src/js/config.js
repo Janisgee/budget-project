3e52bdd91298fd82ad685c9fb3b3a100f883191a
janisgee <janisgeegee@gmail.com>
2023-11-11 22:06:26 +0800

    Add new client url config

--- client/src/js/config.js
+++ client/src/js/config.js
@@ -3,9 +3,9 @@ function getConfig() {
     return {
       SERVER_URL: 'http://localhost:3000',
     };
-  } else if (window.location.hostname === 'somethingelse') {
+  } else if (window.location.hostname === 'money-tracker-vp3w.onrender.com') {
     return {
-      SERVER_URL: 'https://somethingelserandom.com.au',
+      SERVER_URL: 'https://money-tracker-server.onrender.com/',
     };
   }
 }
